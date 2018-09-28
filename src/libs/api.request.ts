df1ad4107ed53c5d70dfd73a97c951bac801ca49
hellomrbigshot <1994yechen@gmail.com>
2018-09-28 14:38:32 +0800

    update

--- src/libs/api.request.ts
+++ src/libs/api.request.ts
@@ -1,6 +1,6 @@
 import HttpRequest from '@/libs/axios'
 import config from '@/config'
 
-// const baseUrl: string = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro;const baseUrl = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro;
-const baseUrl: string = config.baseUrl.dev;
+const baseUrl: string = process.env.NODE_ENV === 'development' ? config.baseUrl.dev : config.baseUrl.pro;
+// const baseUrl: string = config.baseUrl.dev;
 export default new HttpRequest(baseUrl);
