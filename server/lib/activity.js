287a8082e15237189014e5b77fe24cdd29ef8512
hellomrbigshot <1994yechen@gmail.com>
2018-10-27 23:52:01 +0800

    add activity lib

--- server/lib/activity.js
+++ server/lib/activity.js
@@ -1,1 +1,11 @@
 const mongoose = require('./mongo')
+const Scheme = mongoose.Schema
+
+const ActivitySchema = new Schema({
+    create_user: { type: String }, // 创建人
+    create_time: { type: Date }, // 创建时间
+    update_time: { type: Date, default: Date.now }, // 更新时间
+    type: { type: String }, // 动态类型
+})
+
+exports.Activity = mongoose.model('Activity', ActivitySchema)
