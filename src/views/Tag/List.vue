5f2a6a80cec208389bfe77a07c12edffcc824642
hellomrbigshot <1994yechen@gmail.com>
2018-12-14 15:19:47 +0800

    标签列表改版

--- src/views/Tag/List.vue
+++ src/views/Tag/List.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
-        <div style="padding: 10px 15px; color: #999; background: #fff;">当前共有 <span style="color: #555; font-weight: 550;">{{ total }}</span> 个标签</div>
-        <van-pull-refresh v-model="pullLoading" @refresh="onRefresh" style="padding: 5px 15px 15px; background: #fff; margin-top: 10px; min-height: 70vh;">
-            <router-link :to="{ name: 'tagDetail', params: { name: item.name }}" style="padding: 5px 0 15px; border-bottom: 1px dashed #888; display: block;" v-for="(item, index) in tagList" :key="index">
-                <h4 style="font-size: 18px; color: #555; padding: 5px 0;">{{ item.name }}<span style="color: #888;">({{ item.page_num }})</span></h4> 
-                <div style="color: #888; font-size: 12px;">{{ item.description }}</div>
-            </router-link>
+        <div style="padding: 10px 15px; background: #fff; font-size: 14px;">共有 <span style="font-weight: bold;">{{ total }}</span> 个标签</div>
+        <van-pull-refresh v-model="pullLoading" @refresh="onRefresh" style="padding: 10px 8px 0; min-height: 70vh;">
+            <div  v-for="(item, index) in tagList" :key="index" class="tag-wrapper">
+                <router-link :to="{ name: 'tagDetail', params: { name: item.name }}">
+                    <div class="tag-name">{{ item.name }}<span style="color: #888;"></span></div>
+                    <hr style="margin: 0 16px; border: 0; border-bottom: 1px solid #DFE1E5; "/>
+                </router-link>
+                
+                <div class="tag-desc">{{ item.description }}</div>
+                <hr style="margin: 0 16px; border: 0; border-bottom: 1px solid #DFE1E5; "/>
+                <div class="tag-bottom">标签下有 <router-link :to="{ name: 'tagDetail', params: { name: item.name }}" style="color: #1989fa; font-weight: bold;">{{ item.page_num }}</router-link> 篇文章</div>
+            </div>
         </van-pull-refresh>
     </div>
 </template>
@@ -36,4 +42,32 @@ export default class Tags extends Vue {
     }
 }
 </script>
+<style lang="scss" scoped>
+.tag-wrapper {
+    background: #fff;
+    padding: 5px;
+    display: block;
+    margin-bottom: 10px;
+    border-radius: 8px;
+    box-shadow: 0 1px 6px #20212447;
+    .tag-name, .tag-desc, .tag-bottom {
+        padding: 12px 16px;
+        display: block;
+    }
+    .tag-bottom {
+        padding-top: 5px;
+    }
+    .tag-desc, .tag-bottom {
+        color: #888; 
+        font-size: 12px;
+    }
+    .tag-name {
+        font-size: 16px; 
+        color: #1989fa;
+        font-weight: bold;
+        line-height: 20px;
+    }
+}
+</style>
+
 
